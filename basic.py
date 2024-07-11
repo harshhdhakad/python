ed362769b7b975c7dcfec6539fc7d238e5adb17f
harshhdhakad <dhakedharsh91@gmail.com>
2024-07-11 12:14:25 +0530

    Add files via upload
    python

---
+++ basic.py
@@ -0,0 +1,191 @@
+# string
+#name="rohit"
+#print(len(name))     #  len func use for find the lenth of variable
+#new= name.replace("rohit","ritik")  # replace func use for replace the name
+#print(new)
+#upper_var=name.upper()  # upper fucntion for capital the name
+#print(upper_var)
+#name2="RAM"
+#print(name2[0])         # index use for position of name 
+#low_var=name2.lower()  # lower fun for samall the name
+#print(low_var)
+# name is variable name 
+#print(name)
+#intro =""" hello my name is 
+#ram tomar"""
+#print(intro)
+#num1="ram is great"
+#num2="ram is so cute"
+#a="ram"
+#print(num1+" "+num2)
+#found= a in num1
+#print(found)
+# #nun="""Paragraph writing has been a part of the writing process in every student’s life.
+#  Not only for any examination but also in our personal lives, we will need to write about different topics.
+# Paragraph writing is a simple process, and yet it 
+# needs special attention as you have to be short, precise and to the point"""
+
+#group="need"
+#found=group in nun
+#print(found)
+#name="rohit"
+#good=name.replace("rohit","harsh")
+#print(good)
+#name1="LOK ESH"# lower comand are use change to capital to small 
+# lower_var=name1.lower()
+# print(lower_var)
+# print(name1[0:3])
+# print(len(name1))# len function are use to get length of tha char/string
+# print(len(name1))
+# name2=('rohit', '30', 'harsh')
+# print(name2*2)# * is use multiple
+# num="30"
+# found=num in name2
+# print(found)
+# print(len(name2))
+# lst=[1,2,3,4,5,6]
+# lst1=[45678]
+#print(len(lst))
+# num=3
+# found=num in lst
+# print(found)
+#print(lst*4)
+#lst1=[1,2,3,4,4,5,6,7]
+#lst2=[5,6,7,8]
+
+# print(lst1.count(4))
+#print(lst1.append(9,))
+# lst=[1,2,3,4,5,6,]
+# lst.insert(7,8)
+# print(lst)
+# lst.append(9)#add value
+# print(lst)
+# lst.pop()#pop uselast e;iment delete
+
+# print(lst)
+# lst.remove(3)#particuter element delete use 
+# print(lst)
+# lst.reverse()#reverce use ulta karne ke liye
+# print(lst)
+# dct={'name':"ritik",'cls':"second year","rollno":'21'} 
+#name are use : and valye do not use :
+# for key in dct:
+
+    # print(key)
+# for key, value in dct.items():
+#     print(key,value)    
+#string
+# name='rohit'
+# for x in name:
+#     print(x)
+# tpl1=('rohit','harsh',20,'rohan')
+# for x in tpl1:
+#     print(x)
+# set=(1,2,3,4)
+# print(set)
+#opperators 
+# 
+# x=20
+# if(x>10):
+#     print("true")
+# elif(x==5):
+#     print("equale")
+# else:
+#     print("false")
+# x=5
+# print(x)
+# x+=3
+# print(x)
+#x-=2
+#print(x)  
+#x**=2
+#print(x)
+# x=5
+# y=10
+# z=20
+# if x<y and y<z:
+    # print("true")
+# if x<y or y>z:
+#         print("false")
+
+
+# if not x>z:
+#         print("not")
+# x=5
+# y=10
+# z=20
+# if x>y and y>z:
+#     print("true")
+# elif x>y and y>z:
+#     print("grator")
+# else:
+#     print("grator is true")
+# tpl=("rohit",20,40,"kishan")
+# print(len(tpl))
+# dct={'name':"ritik",'cls':"second year","rollno":'21'} 
+# for key in dct:
+#     print(key)
+# for value in dct.values():
+#     print(value)
+# name=(5,10,15,20,25,30,35,40,45,50)
+# for x in name:
+#      print(x)
+# for x in range():
+#     print(name)
+# n=5
+# for x in range(11):
+#     print(n*x)
+a=5  #0101 in anary
+b=3  # 0011 in banary
+# result=(a&b) or and oprator
+# print(result)
+# result=(a|b) or #oprator
+# print(result)
+# result=a^b #exsor oprator
+# print(result)
+# a=[1,2,3,]
+# b=a
+# c=[1,2,3]
+# print(a is b)# is oprator 
+# print(a is c )
+# print(a is not b) #is not oprator 
+# print(a is not c)
+# fruit =["apple","banana","cherry"]
+# name ="apple"
+# print(name in fruit)
+# print(name not in fruit)
+a=2
+# b=5
+# c=8
+# if a==b:
+#     print("true")
+# elif a>b:
+#     print("false")
+# else:
+#     print("good")
+# if a<=2:
+#     print("true")
+#     if a==4:
+#         print("false")
+# else:
+#     print("not found")
+# lst=[1,2,3,4,"rahul","kishan"]
+# for x in lst:
+#   print(x)
+lst=[1,2,3,4,"rahul"]
+for x in lst:
+    print(x)
+    while x<3:
+        print
+
+  
+
+
+
+
+
+
+
+
+
+
